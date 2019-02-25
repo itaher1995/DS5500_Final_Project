0066d534f64d46f5f15c2c60c295b84b6d29c4cf
itaher1995 <ibrahimytaher@gmail.com>
2019-02-24 21:32:38 -0500

    Updated image preprocessing, added data manipulation and analysis, commiting the classes.csv (look up table), the subset lookup and local machine testing script

--- imagePreprocessing.py
+++ imagePreprocessing.py
@@ -13,6 +13,7 @@ CODE IS ADAPTED FROM THE FOLLOWING TUTORIAL:
 import os
 import numpy as np
 import cv2 
+
 from multiprocessing import Pool
 
 def getFiles(dir_):
@@ -21,7 +22,10 @@ def getFiles(dir_):
     INPUT: directory
     OUTPUT: list of image filenames
     '''
-    return [dir_+x for x in os.listdir(dir_)]
+    images = {x:[dir_+x+'/'+y for y in os.listdir(dir_+x)] for x in os.listdir(dir_) if x!='.DS_Store'}
+    images = list(images.values())
+    images = [img for folder in images for img in folder]
+    return images
 
 def imgPreprocessing(imgLoc):
     '''
@@ -32,19 +36,21 @@ def imgPreprocessing(imgLoc):
      OUTPUT: edited image       
     '''
     
-    DIR = 'train2017/'
-    dim = (220,220)
+    DIR = '256_ObjectCategories/'
+    dim = (256,256)
     
     img = cv2.imread(imgLoc,cv2.IMREAD_UNCHANGED) # read in image
-    
+
     res = cv2.resize(img,dim,interpolation=cv2.INTER_LINEAR) # resize to preset dimensions
+    
     
     # gaussian blur inputs:
     # image
     # kernel size
-    blur = cv2.GaussianBlur(res,(5,5),0) # remove noise using gaussian blur
+    blur = cv2.bilateralFilter(res, 9, 75, 75) # remove noise using bilateral blur
     
-    gray = cv2.cvtColor(blur,cv2.COLOR_RGB2GRAY) # convert to grayscale for threshold
+    if len(img.shape)==3:
+        gray = cv2.cvtColor(blur,cv2.COLOR_RGB2GRAY) # convert to grayscale for threshold
     
     # Use adaptive thresholding instead of global thresholding. Assumption is that
     # images are more complex than what can be accomplished using global thresholding
@@ -56,47 +62,24 @@ def imgPreprocessing(imgLoc):
     # size of the kernel
     # constant to be subtracted from weighted mean
     
-    gausThreshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+        gausThreshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
     
+    else:
+        gausThreshold = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
     
-    # Further image processing to make our data as clean as possible
-    # Further noise removal
-    kernel = np.ones((3, 3), np.uint8)
-    opening = cv2.morphologyEx(gausThreshold, cv2.MORPH_OPEN, kernel, iterations=2)
-    
-    # sure background area
-    sure_bg = cv2.dilate(opening, kernel, iterations=3)
-    
-    # Finding sure foreground area
-    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-    ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
-    
-    # Finding unknown region
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg, sure_fg)
-    
-    # Marker labelling, for segmentation
-    ret, markers = cv2.connectedComponents(sure_fg)
-    
-    # Add one to all labels so that sure background is not 0, but 1
-    markers = markers + 1
-    
-    # Now, mark the region of unknown with zero
-    markers[unknown == 255] = 0
-    
-    markers = cv2.watershed(blur, markers)
-    blur[markers == -1] = [255, 0, 0]
-    
-    filename = imgLoc.split('/')[1]
+    filename = imgLoc.split('/')[2]
+
     cv2.imwrite(DIR[:-1]+'_resized/'+filename,gausThreshold)
 
 if __name__=='__main__':
-    DIR = 'train2017/'
+    DIR = '256_ObjectCategories/'
     images = getFiles(DIR)
     
     if DIR[:-1]+'_resized' not in os.listdir():
         os.mkdir(DIR[:-1]+'_resized/')
-    
+
+
     p = Pool(processes=15)
     p.map_async(imgPreprocessing,images)
     p.close()
